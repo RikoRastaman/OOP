76c1a21972eccfef4e1c521c297ed8a7aa82f1b3
RikoRastaman <33201052+RikoRastaman@users.noreply.github.com>
2019-03-25 22:38:18 +0300

    fixed lw3t3

--- lw1_t3/invert/invert/invert.cpp
+++ lw1_t3/invert/invert/invert.cpp
@@ -2,58 +2,103 @@
 #include"pch.h"
 #include<iostream>
 #include<fstream>
-#include <iomanip>
-#include <string>
+#include<iomanip>
+#include<string>
 
 using namespace std;
 const string ZERO_DETERMINANT = "Determinant is zero, matrix can't be inversed";
 const string WRONG_ARGUMENT = "Wrong argument, please enter file with matrix";
+const string FILE_ERROR = "Can't open file";
 
+void readMatrix( float( &mat )[3][3], fstream& input)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			input >> mat[i][j];
+		}
+	}
+}
+
+float calculateDeterminant( float( &mat )[3][3] )
+{
+	float determinant = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		determinant = determinant + (mat[0][i] * (mat[1][(i + 1) % 3] * mat[2][(i + 2) % 3] - mat[1][(i + 2) % 3] * mat[2][(i + 1) % 3]));
+	}
+	return determinant;
+}
+
+void checkDeterminant( float &determinant)
+{
+	if (determinant == 0)
+	{
+		cout << ZERO_DETERMINANT << "\n";
+		exit(1);
+	}
+}
+
+void calculateInverse( float(&mat)[3][3], float &determinant, float(&result)[3][3] )
+{
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			result[i][j] = ((mat[(j + 1) % 3][(i + 1) % 3] * mat[(j + 2) % 3][(i + 2) % 3]) - (mat[(j + 1) % 3][(i + 2) % 3] * mat[(j + 2) % 3][(i + 1) % 3])) / determinant;
+		}
+	}
+}
+
+void printMatrix( float(&matrixToPrint)[3][3] )
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << std::setprecision(3) << matrixToPrint[i][j] << "\t" << std::fixed;
+		}
+		cout << "\n";
+	}
+}
 
 int main(int argc, char* argv[]) 
 {
+
+	float mat[3][3];
+	float result[3][3] = {};
+	int i = 0;
+	int j = 0;
+	float determinant = 0;
+	
+
 	if (argc != 2)
 	{
-		cout << ;
+		cout << WRONG_ARGUMENT;
 		return 1;
 	}
-
 
 	fstream input(argv[1]);
 
 	if(!(input.is_open()))
 	{
-		cout << "Can't open file." << endl;
+		cout << FILE_ERROR << endl;
 		return 1;
 	}
 
-	int mat[3][3];
-	int i = 0;
-	int j = 0;
-	float determinant = 0;
-
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			input >> mat[i][j];
-
-	for (i = 0; i < 3; i++)
-		determinant = determinant + (mat[0][i] * (mat[1][(i + 1) % 3] * mat[2][(i + 2) % 3] - mat[1][(i + 2) % 3] * mat[2][(i + 1) % 3]));
-
-	//cout << "\n\nDeterminant: " << determinant<< endl;
-
-	if (determinant == 0)
-	{
-		cout << ZERO_DETERMINANT << "\n";
-		return 1;
-	}
-
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++)
-			cout << std::setprecision(3) << ((mat[(j + 1) % 3][(i + 1) % 3] * mat[(j + 2) % 3][(i + 2) % 3]) - (mat[(j + 1) % 3][(i + 2) % 3] * mat[(j + 2) % 3][(i + 1) % 3])) / determinant << "\t";
-		std::cout << std::fixed;
-
-		cout << "\n";
-	}
-
+	readMatrix(mat, input);
+	determinant = calculateDeterminant(mat);
+	checkDeterminant(determinant);
+	calculateInverse(mat, determinant, result);
+	printMatrix(result);
 	return 0;
 }
+/*
+for (j = 0; j < 3; j++)
+	cout << std::setprecision(3) << ((mat[(j + 1) % 3][(i + 1) % 3] * mat[(j + 2) % 3][(i + 2) % 3]) - (mat[(j + 1) % 3][(i + 2) % 3] * mat[(j + 2) % 3][(i + 1) % 3])) / determinant << "\t";
+std::cout << std::fixed;
+
+cout << "\n";
+	}
+	*/
---
+++ lw2_t1/vector/vector/vector.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <iomanip>
+
+using namespace std;
+
+void readFromInput(vector<float>& massivForRead)
+{
+	float number;
+	while ( cin >> number)
+	{
+		massivForRead.push_back(number);
+	}
+}
+
+void printMassiv(vector<float>& massivForPrint)
+{
+
+	std::cout << fixed << std::setprecision(3);
+	std::copy(
+	massivForPrint.begin(),
+	massivForPrint.end(),
+	std::ostream_iterator<float>(std::cout, " ")
+	);
+
+}
+
+float findMinimum(vector<float>& massiv)
+{
+	if (massiv.size() != 0)
+	{
+		return *std::min_element(massiv.begin(), massiv.end());
+	}
+}
+
+void multiplicMassiv(vector<float>& massiv, float multiplicator)
+{
+	for (int i = 0; i < massiv.size(); ++i)
+	{
+		massiv[i] = massiv[i] * multiplicator;
+	}
+}
+
+int main()
+{
+	vector<float> massiv;
+
+	readFromInput(massiv);
+	findMinimum(massiv);
+	multiplicMassiv(massiv, findMinimum(massiv));
+	printMassiv(massiv);
+}
