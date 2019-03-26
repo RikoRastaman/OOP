6c535e5714a4fd2c363a0e4d7b9e0629e9798617
RikoRastaman <33201052+RikoRastaman@users.noreply.github.com>
2019-03-26 10:58:02 +0300

    Edite lw1t3

--- lw1_t3/invert/invert/Test.bat
+++ lw1_t3/invert/invert/Test.bat
@@ -5,6 +5,17 @@ set PROGRAM="%~1"
 set OUT="%TEMP%\out.txt"
 
 %PROGRAM% input_1st.txt > %OUT%
-fc %OUT% output_1st_matrix.txt
+fc %OUT% output_1st_matrix.txt || goto err
 
+%PROGRAM% input_zero.txt > %OUT%
+fc %OUT% output_zero.txt || goto err
+
+%PROGRAM% > %OUT%
+fc %OUT% output_argument_error.txt || goto err
+
+echo "Tests passed!"
 exit /B 0
+
+:err
+echo Testing failed
+exit /B 1
--- lw1_t3/invert/invert/invert.cpp
+++ lw1_t3/invert/invert/invert.cpp
@@ -5,12 +5,11 @@
 #include<iomanip>
 #include<string>
 
-using namespace std;
-const string ZERO_DETERMINANT = "Determinant is zero, matrix can't be inversed";
-const string WRONG_ARGUMENT = "Wrong argument, please enter file with matrix";
-const string FILE_ERROR = "Can't open file";
+const std::string ZERO_DETERMINANT = "Determinant is zero, matrix can't be inversed";
+const std::string WRONG_ARGUMENT = "Wrong argument, please enter file with matrix";
+const std::string FILE_ERROR = "Can't open file";
 
-void readMatrix( float( &mat )[3][3], fstream& input)
+void readMatrix( float( &mat )[3][3], std::fstream& input)
 {
 	for (int i = 0; i < 3; i++)
 	{
@@ -35,7 +34,7 @@ void checkDeterminant( float &determinant)
 {
 	if (determinant == 0)
 	{
-		cout << ZERO_DETERMINANT << "\n";
+		std::cout << ZERO_DETERMINANT << "\n";
 		exit(1);
 	}
 }
@@ -57,36 +56,40 @@ void printMatrix( float(&matrixToPrint)[3][3] )
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cout << std::setprecision(3) << matrixToPrint[i][j] << "\t" << std::fixed;
+			std::cout << std::setprecision(3) << matrixToPrint[i][j] << "\t" << std::fixed;
 		}
-		cout << "\n";
+		std::cout << "\n";
+	}
+}
+
+void checkArguments(int argc)
+{
+	if (argc != 2)
+	{
+		std::cout << WRONG_ARGUMENT;
+		exit(1);
+	}
+}
+
+void checkFile(std::fstream& input)
+{
+	if (!(input.is_open()))
+	{
+		std::cout << FILE_ERROR << "\n";
+		exit(1);
 	}
 }
 
 int main(int argc, char* argv[]) 
 {
-
-	float mat[3][3];
+	float mat[3][3] = {};
 	float result[3][3] = {};
 	int i = 0;
 	int j = 0;
-	float determinant = 0;
-	
-
-	if (argc != 2)
-	{
-		cout << WRONG_ARGUMENT;
-		return 1;
-	}
-
-	fstream input(argv[1]);
-
-	if(!(input.is_open()))
-	{
-		cout << FILE_ERROR << endl;
-		return 1;
-	}
-
+	float determinant = 0;	
+	checkArguments(argc);
+	std::fstream input(argv[1]);
+	checkFile(input);
 	readMatrix(mat, input);
 	determinant = calculateDeterminant(mat);
 	checkDeterminant(determinant);
@@ -94,11 +97,3 @@ int main(int argc, char* argv[])
 	printMatrix(result);
 	return 0;
 }
-/*
-for (j = 0; j < 3; j++)
-	cout << std::setprecision(3) << ((mat[(j + 1) % 3][(i + 1) % 3] * mat[(j + 2) % 3][(i + 2) % 3]) - (mat[(j + 1) % 3][(i + 2) % 3] * mat[(j + 2) % 3][(i + 1) % 3])) / determinant << "\t";
-std::cout << std::fixed;
-
-cout << "\n";
-	}
-	*/
